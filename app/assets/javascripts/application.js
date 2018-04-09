2bdcf27abb720edfcccbeaf5884b242e3105b9be
ankita dangi <ankita.dangi@zingbytes.com>
2018-04-09 18:55:53 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require rails-ujs
+//= require ckeditor/init
+//= require bootstrap-sprockets
+//= require sidebar-nav.min
+//= require jQuery.style.switcher
+//= require bootstrap-select
+//= require moment.min
+//= require fullcalendar
+//= require bootstrap-datepicker
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ namespace :admin do
+ 	resources :departments
+ 	get "home/index"
+ 	root "home#index"
+ end
+end
